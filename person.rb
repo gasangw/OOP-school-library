6a092c707b7c3c7baaad7a6ede56d11495ef0bb5
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-09-07 15:31:43 +0200

    make the has to many, belong to many and many to many relationships

--- person.rb
+++ person.rb
@@ -1,14 +1,15 @@
 require './nameable'
 class Person < Nameable
+  attr_reader :rental, :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
     super()
   end
-
-  attr_reader :id
   attr_accessor :name, :age
 
   def of_age?
