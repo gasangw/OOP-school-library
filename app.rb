d9f5bfa4c1862a74ca962d02df96030ddf078088
Gasangwa Thomas <thomasgasangwa@gmail.com>
2022-09-09 11:37:37 +0200

    work on functionality

--- app.rb
+++ app.rb
@@ -1,61 +1,85 @@
-require './classroom'
-require './book'
-require './student'
-require './teacher'
+require_relative 'book'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'rental'
+
 
 class App
-  attr_reader :books, :classrooms, :students, :teachers
-
   def initialize
-    @students = []
     @books = []
-    @classrooms = []
+    @people = []
     @rentals = []
-    @teachers = []
   end
 
-  def list_books(_books)
-    @books.each { |b| puts "[Book] Title: #{b.title}, Author: #{b.author}" }
+  def list_all_books
+    if @books.empty?
+      puts 'There is no book!'
+    else
+      @books.each { |book| puts "Title: '#{book.title}', Author: #{book.author}" }
+    end
   end
 
-  def list_classroom
-    @classrooms.each { |c| puts "[Classroom] Label: #{c.label}" }
+  def list_all_people
+    if @people.empty?
+      puts 'there is no person'
+    else
+      @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    end
   end
 
-  def list_people
-    @students.each { |s| puts "[Student] ID: #{s.id}, Name: #{s.name}" }
-    @teachers.each { |t| puts "[Teacher] ID: #{t.id}, Name: #{t.name}" }
+  def create_person
+    puts 'do you want to create a student(1) or a teacher (2):'
+    selected_person = gets.chomp.to_i
+    print 'age:'
+    age = gets.chomp.to_i
+    print 'name:'
+    name = gets.chomp.to_s
+    case selected_person
+    when 1
+      print 'Has parent permission? [Y/N]: '
+      provided_permission = gets.chomp.capitalize
+      student_permission = true if provided_permission == 'Y'
+      student_permission = false if provided_permission == 'N'
+      @people.push(Student.new(nil, age, name, parent_permission: student_permission))
+    when 2
+      print 'Specialization: '
+      specialization = gets.chomp.to_s
+      @people.push(Teacher.new(age, name, specialization))
+    end
+    puts 'Person created successfully.'
   end
 
-  def create_student(age, name, classroom)
-    student = Student.new(age, name, classroom)
-    @student << student
-    student
+  def create_book
+    print 'title:'
+    title = gets.chomp
+    print 'author:'
+    author = gets.chomp
+    @books.push(Book.new(title, author))
+    puts 'the book is created successfully'
   end
 
-  def create_teacher(age, name, specialization)
-    teacher = Teacher.new(age, name, specialization)
-    @teacher << teacher
-    teacher
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.map.with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
+    selected_book = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (Not ID): '
+    @people.map.with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    selected_person = gets.chomp.to_i
+    print 'date:'
+    provided_date = gets.chomp
+    @rentals.push(Rental.new(provided_date, @books[selected_book], @people[selected_person]))
+    puts ' rental created succesfully'
   end
 
-  def create_book(title, author)
-    book = Book.new(title, author)
-    @book << book
-    book
-  end
-
-  def create_rental(person, book)
-    rental = Rental.new(person: person, book: book)
-    @rentals << rental
-    rental
-  end
-
-  def rentals_by_person(person_id)
-    (@rentals.select do |r|
-       r.person_id = person_id
-     end).each do |d|
-      puts "[Rental] Date: #{d.date}, Title: #{d.book.title}, Borrower: #{d.person.name}"
+  def list_all_rentals_by_id
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+    (@rentals.select { |rental| rental.person.id == person_id }).each do |rental|
+      puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,30 +1,17 @@
-require './app'
+require './choice'
 
-class Main < App
-  def run
-    puts 'Your most welcome to OOP School Library'
-    loop do
-      list
-      option = gets.chomp
-
-      break if option == '6'
-      # option.gsub(/[^0-9]/, '')
-      # get_num option
+def main
+  choice = Choice.new
+  loop do
+    choice.display_list
+    option = gets.chomp.to_i
+    case option
+    when 1..6
+      choice.dispatch(option)
+    else
+      break
     end
-    puts 'Thanks for visiting our library'
   end
-
-  def list
-    puts
-    puts 'Kindly select an option by entering a number'
-    puts '0 - List all books'
-    puts '1 - List all people'
-    puts '2 - Create a person'
-    puts '3 - Create a book'
-    puts '4 - Create a rental'
-    puts '5 - List all rentals for a given person id'
-    puts '6 - Exit'
-  end
+  choice.on_exit
 end
-
-Main.new.run
+main
