ad26b8e1492739bb44d9d73fe7b488ef4f8bef55
petushka1 <nadya@devery.io>
2022-09-14 14:02:15 +0300

    add JSON writer for books

--- prosessor.rb
+++ prosessor.rb
@@ -2,13 +2,11 @@ require 'json'
 
 module Prosessor
     def books_to_file
-   
-   book_obj = @books.map do |book| 
-    { :title => book.title, :author => book.author } 
-   end
-   JSON.dump(File.write('./library/books.json', book_obj))
-   
-   # json = JSON.generate({@books['title'], books['name']})
-   # File.open('./library/books.json', 'w')
+        def books_to_file
+            book_obj = @books.map do |book|
+              { title: book.title, author: book.author }
+            end
+            File.write('./library/books.json', book_obj.to_json)
+          end
     end
 end
