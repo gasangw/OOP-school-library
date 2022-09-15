ebe68e9c9495d3f5e1939759e00923cd40c412b5
Abby Nyakara <abbynyakara@gmail.com>
2022-09-15 13:21:04 +0300

    add the tests for solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,19 @@
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context "To test the solver class" do
+    it "Returns the factorial of the number" do
+      expect(@solver.factorial(5)).to be 120
+    end
+
+    it "Reverses the String input" do
+      expect(@solver.reverse("abby")).to eql "ybba"
+    end
+
+    it "Runs the fizzbuzz algorithm" do
+      expect(@solver.fizzbuzz(15)).to eq 'FizzBuzz'
+    end
+  end
+end
